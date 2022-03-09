b0bcea791a61b2239c3977097c6544d7a4ca1f72
Pietro Liguori <pietro_liguori@hotmail.com>
2022-03-08 23:05:24 -0300

    Project created

---
+++ src/main/java/com/vili/sorsfinance/SorsfinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.vili.sorsfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorsfinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorsfinanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vili/sorsfinance/SorsfinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vili.sorsfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorsfinanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
